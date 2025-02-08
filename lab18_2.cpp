821882e28ceb3ea4fc047a85817459b57ff21cb3
mamamudang <humlake7.66@gmail.com>
2025-02-08 15:19:41 +0700

    lab18_2.cpp completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+	double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect* R1, Rect* R2) {
+	double x_overlap = max(0.0, min(R1->x + R1->w, R2->x + R2->w) - max(R1->x, R2->x));
+	double y_overlap = max(0.0, min(R1->y, R2->y) - max(R1->y - R1->h, R2->y - R2->h));
+	return x_overlap * y_overlap;
+}
 
+int main() {
+	Rect R1 = {1, 5, 5, 5};
+	Rect R2 = {2, 6, 5, 5};
 
-
+	cout << overlap(&R1, &R2);
+	return 0;
 }
